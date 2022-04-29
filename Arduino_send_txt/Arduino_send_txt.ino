c5e734596ef7568dac0a6555eaf23d0cc0d4818c
Trevor Nganyi <77149600+LT-Nganyi@users.noreply.github.com>
2022-04-29 12:01:14 +0300

    Arduino communication with python

---
+++ Arduino_send_txt/Arduino_send_txt.ino
@@ -0,0 +1,15 @@
+String x;
+String reply;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+}
+
+void loop() {
+  while (!Serial.available());
+  //x = Serial.readString().toInt();
+  x = Serial.readString();
+  reply = "String Received: " + x ;
+  Serial.print(reply);
+}
